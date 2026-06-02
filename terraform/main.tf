026367993a076335bdcb2abd044970cd06dcb474
singhayush61 <ayushofficialsv@gmail.com>
2026-06-02 20:04:07 +0530

    feat: add VPC, subnet, and security group resources with EC2 instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,90 @@
-resource "aws_instance" "devops-project-terraform" {
-
-  ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t3.micro"
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "devops-project"
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "devops-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "devops_sg" {
+  name   = "devops-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0685bcc683dadb6b9"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install docker -y
+                systemctl start docker
+                systemctl enable docker
+
+                docker run -d \
+                --restart unless-stopped \
+                -p 5000:5000 \
+                ayushsingh0/devops-app:v1
+                EOF
+
+  tags = {
+    Name = "devops-server"
   }
 }
