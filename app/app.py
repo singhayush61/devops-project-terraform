18781083ae7435d6411872329ef85b20610f61b3
singhayush61 <ayushofficialsv@gmail.com>
2026-06-02 20:07:46 +0530

    feat: implement voting functionality with Flask and add HTML template

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+votes = {
+    "Terraform": 0,
+    "Docker": 0
+}
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello from DevOps Project"
+    if request.method == "POST":
+        choice = request.form.get("vote")
+        votes[choice] += 1
+
+    return render_template("index.html", votes=votes)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
